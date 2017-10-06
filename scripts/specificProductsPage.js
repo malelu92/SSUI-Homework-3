247f7a314edced689751f1330500907940d6bd44
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-06 02:02:45 -0400

    color choice working with libs

--- scripts/specificProductsPage.js
+++ scripts/specificProductsPage.js
@@ -2,3 +2,23 @@ function addToCart() {
 	var qty = $("#quantity").find(":selected").text();
 	document.getElementById("qtyItems").innerHTML = qty + " items"
 }
+
+function selectColor() {
+	console.log("faf")
+	$(".highlight").addClass("highlight");
+	$(this).addClass("highlight");
+}
+
+/*document.getElementsByClassName('colorChoice')[0]
+        .addEventListener('click', function (event) {
+            alert("lala")
+        });*/
+
+$(document).ready(function(){
+	$('.colorChoice').click(function(){
+		$('.colorChoice').removeClass("highlight");
+		$(".highlight").addClass("highlight");
+		$(this).addClass("highlight");
+	});
+
+});
