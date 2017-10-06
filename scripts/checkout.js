32bd2c8427ce7a3eaa942a4efa907c872967f20f
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-06 02:37:54 -0400

    removing item from cart per id

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
-
-  function {
-    alert.("hi");
-  }
+$(document).ready(function(){
+	document.getElementById("removeItem").onclick = function(){
+		var qty = document.getElementById("qtyItems").textContent;
+		var qtyValue = parseInt(qty.substr(0,qty.indexOf(' ')));
+		if (qtyValue > 0) {
+			var inCart = qtyValue-1;
+			document.getElementById('qtyItems').innerHTML = inCart + ' items';
+		}
 }
+});
--- scripts/specificProductsPage.js
+++ scripts/specificProductsPage.js
@@ -1,24 +1,12 @@
 function addToCart() {
-	var qty = $("#quantity").find(":selected").text();
-	document.getElementById("qtyItems").innerHTML = qty + " items"
+	var qty = $('#quantity').find(':selected').text();
+	document.getElementById('qtyItems').innerHTML = qty + ' items';
 }
-
-function selectColor() {
-	console.log("faf")
-	$(".highlight").addClass("highlight");
-	$(this).addClass("highlight");
-}
-
-/*document.getElementsByClassName('colorChoice')[0]
-        .addEventListener('click', function (event) {
-            alert("lala")
-        });*/
 
 $(document).ready(function(){
 	$('.colorChoice').click(function(){
-		$('.colorChoice').removeClass("highlight");
-		$(".highlight").addClass("highlight");
-		$(this).addClass("highlight");
+		$('.colorChoice').removeClass('highlight');
+		$('.highlight').addClass('highlight');
+		$(this).addClass('highlight');
 	});
-
 });
