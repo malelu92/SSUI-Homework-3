3af3a36bc2825eb07c58b6c4ad1d6eeb206aafad
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-07 14:36:49 -0400

    adding vanilla javascript to specific products page - capturing selected items

--- scripts/specificProductsPage.js
+++ scripts/specificProductsPage.js
@@ -1,12 +1,36 @@
-function addToCart() {
-	var qty = $('#quantity').find(':selected').text();
-	document.getElementById('qtyItems').innerHTML = qty + ' items';
+function addToCart(){
+    var selectTag = document.getElementsByTagName("select");
+    var totalTags = selectTag.length;
+    var output = "";
+    for(var i=0; i<totalTags; i++){
+        output += selectTag[i].name + ":\n";
+        for(var j=0, lenj=selectTag[i].options.length; j<lenj; j++){
+        	if(selectTag[i].options[j].selected) {
+            output += "\t- " 
+                   //+ s[i].options[j].value 
+                   + " (" + selectTag[i].options[j].text + "): " 
+                   + (selectTag[i].options[j].selected ? 'Yes' : 'No') 
+                   + "\n";
+            }
+        }
+    }
+    alert(output);
 }
 
-$(document).ready(function(){
+/*function addToCart() {
+	//alert("lala")
+	//var qty = $('#quantity').find(':selected').text();
+	var qty = document.getElementById('quantity').innerHTML;
+	console.log(qty.option.value)
+
+	console.log(qty)
+	//document.getElementById('qtyItems').innerHTML = qty + ' items';
+}*/
+
+/*$(document).ready(function(){
 	$('.colorChoice').click(function(){
 		$('.colorChoice').removeClass('highlight');
 		$('.highlight').addClass('highlight');
 		$(this).addClass('highlight');
 	});
-});
+});*/
