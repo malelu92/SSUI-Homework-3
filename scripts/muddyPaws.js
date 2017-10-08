4195607d09047c0eb7938642f3ca01d0cabe2f6c
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-08 16:07:42 -0400

    code done

--- scripts/muddyPaws.js
+++ scripts/muddyPaws.js
@@ -1,3 +1,4 @@
+/* Gets size, quantity and color of selected product and adds to cart. */
 function addToCart(){
 	size = getProductSelectChoice('size');
 	qty = parseInt(getProductSelectChoice('quantity'));
@@ -6,6 +7,7 @@ function addToCart(){
 	updateShoppingCart(qty)
 }
 
+/* Gets chosen element from select bar. */
 function getProductSelectChoice(id) {
     var selectTag = document.getElementById(id);
     var totalValues, size;
@@ -18,12 +20,14 @@ function getProductSelectChoice(id) {
         }
 }
 
+/* Gets total number of items in shopping cart. */
 function getTotalItemsCart () {
 	var cartText = document.getElementById('shopping-cart-text').innerHTML;
 	var qtyItems = cartText.substr(0,cartText.indexOf(' '));
 	return parseInt(qtyItems)
 }
 
+/* Removes one item from shopping cart. */
 function removeFromCart(obj) {
 	var items = getTotalItemsCart();
 	var elem;
@@ -35,7 +39,7 @@ function removeFromCart(obj) {
 	}
 }
 
-
+/* Updates shopping card number of items with given quantitiy. */
 function updateShoppingCart(addQty) {
 	var totalItems = getTotalItemsCart() + addQty;
 	document.getElementById('shopping-cart-text').innerHTML = totalItems + ' items';
